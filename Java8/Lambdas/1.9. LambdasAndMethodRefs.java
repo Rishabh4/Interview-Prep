b4a565aeea09cc0928b0f5e254b8ae58a34ea775
Rishabh Jain <rishabh.sre.92@gmail.com>
2024-08-22 21:00:53 +0530

    Added Lambdas

---
+++ Java8/Lambdas/1.9. LambdasAndMethodRefs.java
@@ -0,0 +1,30 @@
+package lets_get_certified.lambdas;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class LambdasAndMethodRefs {
+    public static void main(String[] args) {
+        staticMR();
+        boundMR();
+        unboundMR();
+        constructorMR();
+    }
+
+    static void staticMR() {
+
+    }
+
+    static void boundMR() {
+
+    }
+
+    static void unboundMR() {
+
+    }
+
+    static void constructorMR() {
+
+    }
+}
